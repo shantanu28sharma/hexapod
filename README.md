92353d1cb81cac90a076d5ec1667a25e3b52780c
shantanu28sharma <shantanu28sharma@gmail.com>
2021-01-23 22:33:55 +0530

    info

---
+++ README.md
@@ -0,0 +1,5 @@
+### 12 DOF Hexapod Robot using tripod gait for walking
+
+
+Here is the sample 3d plot
+<img src="./images/sample.png" alt="3d Plot" style="width:1000px;height:200px;"/>
--- main.py
+++ main.py
@@ -164,29 +164,6 @@ class Plotter:
         mpl.rcParams['legend.fontsize'] = 8
         fig = plt.figure()
         ax = fig.gca(projection='3d')
-        # theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-        # z = np.linspace(-2, 2, 100)
-        # r = z**2 + 1
-        # x = r * np.sin(theta)
-        # y = r * np.cos(theta)
-        # m = 10
-        # f = 5
-        # s = 15
-        # p1 = [m, f, -f, -m, -f, f, m]
-        # p2 = [0, s, s, 0, -s, -s, 0]
-        # p3 = [0, 0, 0, 0, 0, 0, 0]
-        # ax.plot(p1, p2, p3, label='parametric curve')
-        # p1 = [0, 10]
-        # p2 = [0, 10]
-        # p3 = [0, 0]
-        # ax.plot(p1, p2, p3, label='parametric curve')
-        # ax.legend()
-
-        # line_1  = plt.plot([1, 2, 3])
-        # line_2  = plt.plot([2, 4, 6])
-        # line = line_2.pop(0)
-        # line.remove()
-        # https://www.kite.com/python/answers/how-to-remove-a-line-from-a-plot-in-python
         for i in range(len(points[0])):
             px = points[0][i]
             py = points[1][i]
@@ -232,9 +209,7 @@ class Plotter:
 if __name__ == "__main__":
     hex = HexaPod((0, 0, 0), 10, 5, 15, ())
     res = hex.get_cords()
-    # print(res[3])
-    stamps = hex.simulate_tripod(20, 20)
-    # print(stamps)
-    Plotter.plot_frames(stamps)
-    # Plotter.plot(res)
+    # stamps = hex.simulate_tripod(20, 20)
+    # Plotter.plot_frames(stamps)
+    Plotter.plot(res)
     
